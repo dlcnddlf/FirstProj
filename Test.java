d7d045d502a79d75ac0faa62b17a852e33890416
chungil.lee <sn7679@naver.com>
2021-12-21 09:55:48 +0900

    주석후 변경

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class test{
 	public static void main(String [] args){
-		//System.out.println("Heo");
+		//System.out.println("wow");
 		System.out.println("Git");
 		System.out.println("Heo Git");
 		System.out.println("Hi Git");
